e7381c9bbcbe69052c04598449a6d729f50c7d29
m-skrskr <mirta.krajacic@fer.hr>
2025-08-03 22:00:22 +0200

    Spajanje s bazom

--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
-const express = require('express')
-const app = express() // kreairamo express aplikaciju -> to je moj server
+import express from 'express';
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
-app.get("/api", (req, res) => { // kad netko posalje get zahtjev na moj server odgovor ce biti json u res
+dotenv.config();
+const { Pool } = pkg;
+const app = express();
+
+const pool = new Pool(); // reuses existing connections instead of opening a new one for every query
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
+
+// proba jesmo li se spojili na database
+try {
+  await pool.query('SELECT 1');
+  console.log('Connected to the database!');
+} catch (err) {
+  console.error('Connection error', err.stack);
+}
+
+// fetching korisnika
+try {
+    const res = await pool.query('SELECT * FROM korisnik');
+    console.log('Fetched users:', res.rows[0]);
+} catch (err) {
+    console.error('error');
+}
+
+app.get("/", (req, res) => { 
     res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
-
-app.listen(5000, () => {console.log("server started on port 5000")});
+});
