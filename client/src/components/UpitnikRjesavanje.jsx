81e10492188c06bd929fae9f12d49369848ca94a
m-skrskr <mirta.krajacic@fer.hr>
2025-08-30 21:01:42 +0200

    Rjesavanje upitnika UI update

--- client/src/components/UpitnikRjesavanje.jsx
+++ client/src/components/UpitnikRjesavanje.jsx
@@ -1,5 +1,5 @@
 // element upitnika dobiven parsiranjem uploadanog XML-a
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Upitnik({ xmlData, rezultatSpecs, setBodoviRoditelj, naslov }) {
   const [bodoviPitanja, setBodoviPitanja] = useState({}); // entry oblika: {varName:bodovi}
@@ -28,83 +28,120 @@ function Upitnik({ xmlData, rezultatSpecs, setBodoviRoditelj, naslov }) {
       return (
         <div className="p-3 d-flex flex-column flex-md-row justify-content-md-evenly">
           {cats.map((c) => (
-            <div key={`${q.varName}-${c.value}`}  className="form-check me-md-3">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name={q.response.varName}
-                  value={
-                    obrnutoKodirani.includes(q.varName)
+            <div key={`${q.varName}-${c.value}`} className="form-check me-md-3">
+              <input
+                className="form-check-input"
+                type="radio"
+                name={q.response.varName}
+                value={
+                  obrnutoKodirani.includes(q.varName)
+                    ? min + max - parseInt(c.value)
+                    : parseInt(c.value)
+                }
+                id={`${q.varName}-${c.value}`}
+                checked={
+                  q.varName in bodoviPitanja &&
+                  bodoviPitanja[q.varName] ===
+                    (obrnutoKodirani.includes(q.varName)
                       ? min + max - parseInt(c.value)
-                      : parseInt(c.value)
-                  }
-                  id={`${q.varName}-${c.value}`}
-                  checked={
-                    q.varName in bodoviPitanja &&
-                    bodoviPitanja[q.varName] ===
-                      (obrnutoKodirani.includes(q.varName)
-                        ? min + max - parseInt(c.value)
-                        : parseInt(c.value))
-                  }
-                  onChange={(e) => {
-                    setBodoviPitanja((prev) => {
-                      let temp = { ...prev };
-                      temp[q.varName] = parseInt(e.target.value);
-                      return temp;
-                    });
-                    setBodoviRoditelj((prev) => {
-                      let temp = { ...prev };
-                      temp[q.varName] = parseInt(e.target.value);
-                      return temp;
-                    });
-                  }}
-                />
-                <label
-                  className="form-check-label text-muted"
-                  htmlFor={`${q.varName}-${c.value}`}
-                >
-                  {c.label}
-                </label>
-              </div>
-            ))}
+                      : parseInt(c.value))
+                }
+                onChange={(e) => {
+                  setBodoviPitanja((prev) => {
+                    let temp = { ...prev };
+                    temp[q.varName] = parseInt(e.target.value);
+                    return temp;
+                  });
+                  setBodoviRoditelj((prev) => {
+                    let temp = { ...prev };
+                    temp[q.varName] = parseInt(e.target.value);
+                    return temp;
+                  });
+                }}
+              />
+              <label
+                className="form-check-label text-muted"
+                htmlFor={`${q.varName}-${c.value}`}
+              >
+                {c.label}
+              </label>
+            </div>
+          ))}
         </div>
       );
     }
     return <em>Unsupported response</em>;
   }
 
-  function ListaPitanja({ data }) {
-    const sections = arr(data.section); // section = sekcija pitanja u queXML
+  function ListaPitanja({ question, subQs, startInd }) {
+    return (
+      <section>
+        {subQs.map((sq, ind) => (
+          <div key={sq.varName} className="mt-4">
+            <span className="fw-semibold">
+              {startInd + ind + 1 + ". " + sq.text}
+            </span>
 
-    return sections.map((sec, i) => {
-      const q = sec.question;
-      const subQs = arr(q.subQuestion);
-
-      return (
-        <section key={i}>
-          {subQs.map((sq, ind) => (
-            <div key={sq.varName} className="mt-4">
-              <span className="fw-semibold">{ind + 1 + ". " + sq.text}</span>
-
-              <Response
-                q={{
-                  ...sq,
-                  response: {
-                    ...q.response, // zelimo da svaki subQuestion ima svoj nezavisni response objekt
-                    varName: sq.varName, // i svoj varName
-                  },
-                }}
-              />
-            </div>
-          ))}
-        </section>
-      );
-    });
+            <Response
+              q={{
+                ...sq,
+                response: {
+                  ...question.response, // zelimo da svaki subQuestion ima svoj nezavisni response objekt
+                  varName: sq.varName, // i svoj varName
+                },
+              }}
+            />
+          </div>
+        ))}
+      </section>
+    );
   }
+
+  const data = xmlData.questionnaire;
+  const sec = data.section; // section = sekcija pitanja u queXML
+  const q = sec.question;
+  const subQs = arr(q.subQuestion);
+
+  const pageSize = 5;
+  const ukupnoPages = Math.ceil(subQs.length / pageSize);
+  const [page, setPage] = useState(0);
+  const [currPitanja, setCurrPitanja] = useState(subQs.slice(0, 5));
+
+  useEffect(() => {
+    setCurrPitanja(subQs.slice(pageSize * page, pageSize * page + 5));
+  }, [page]);
 
   return (
     <div className="container">
-      <ListaPitanja data={xmlData.questionnaire} />
+      <ListaPitanja
+        question={q}
+        subQs={currPitanja}
+        startInd={pageSize * page}
+      />
+
+      <div className="d-flex justify-content-center align-items-center mt-4">
+        <div className="d-flex justify-content-center align-items-center gap-3 my-3">
+          <button
+            className="btn btn-arrow px-3 py-1"
+            disabled={page === 0}
+            onClick={() => setPage((p) => p - 1)}
+          >
+            ‹
+          </button>
+
+          <span className="fw-semibold text-primary">
+            Stranica {page + 1} / {ukupnoPages}
+          </span>
+
+          <button
+            className="btn btn-arrow px-3 py-1"
+            disabled={page + 1 === ukupnoPages}
+            onClick={() => setPage((p) => p + 1)}
+          >
+            ›
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
